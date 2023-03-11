50202d3c364e683adc97b3058348940ca9bffd3c
Batuhan Kacmaz <batuhan.kacmaz1105@gmail.com>
2023-03-11 16:58:18 +0300

    initial project commit

--- CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardSelectionVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CardSelectionVC.swift
 //  CardWorkout
 //
 //  Created by Batuhan Kacmaz on 11.03.2023.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CardSelectionVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        view.backgroundColor = .brown
+    }
+    
 
 }
-
